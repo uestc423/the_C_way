08142f2e5a6608fba78f6b6d665f085b30856ad7
Tao Heng <uestcheng@gmail.com>
2013-06-09 17:07:45 +0800

    加入了内存检测
    加入了内存检测和加入了新的元素 connector_cell_id(CCI)

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define INACTIVE 0
 #define ISACTIVED 1
 #define ISPREDICTED 2
-#define SIZE 16 * 16
+#define SIZE 16000 * 16000
 #define FIRST 1
 
 //The initial of cell
@@ -13,6 +14,7 @@ typedef struct cell
     int column;
     int status;
     int ID;
+    int CCI;                //connector_cell_id
 
 }Matrix, *pMatrix;
 
@@ -22,25 +24,22 @@ void show_cell_status(pMatrix);
 
 int main(void)
 {
-<<<<<<< HEAD
+
     //define a pointer;
     pMatrix p;
     //set a 16 * 16 memory
     p = (pMatrix)malloc(SIZE * sizeof(Matrix));
-=======
-    //define a struct cell pointer
-    struct cell * matrix;
-    //malloc a memory
-
-    matrix = (struct cell *)malloc(SIZE * sizeof(struct cell));
->>>>>>> f39ecf766464acd001e4d1e9af37ce6c819b6434
-
-    cell_initial(matrix);
-    show_cell_status(matrix);
+    if(p)                        //evaluate the pointer is exist
+    {
+        cell_initial(p);
+        show_cell_status(p);
+    }
+    else
+    {
+        printf("memory is null");
+    }
     return 0;
 }
-
-
 
 //the initial function of cell
 void cell_initial(pMatrix ptr)
