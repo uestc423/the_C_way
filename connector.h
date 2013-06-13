162e31d85847d603def6345d201dd6e9cbd3fcc9
Tao Heng <uestcheng@gmail.com>
2013-06-13 10:55:17 +0800

    格式调整
    调整了下格式

--- connector.h
+++ connector.h
@@ -5,17 +5,9 @@
 typedef struct connector
 {
     int weight;
-<<<<<<< HEAD
     int local_id;               //本地ID由columnid和cellid组成。
     int target_id;              //目标ID由columnid和cellid组成。
 }Connector, *pConnector;
 
 
 #endif
-=======
-    int column_id;
-}Connector, *pConnector;
-
-
-#endif
->>>>>>> e34ca618ee6978556d0a90a80b508133e96d83cf
