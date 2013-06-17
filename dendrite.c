ed0a4c713edebae5eb3b7bbde18ececcc74e0670
Tao Heng <uestcheng@gmail.com>
2013-06-17 11:48:06 +0800

    修正错误
    修正了一些拼写错误

--- dendrite.c
+++ dendrite.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "dendrite.h"
+#include "define.h"
 #define DPN SIZE / 4
 #define DDN SIZE * 4
 
-void proximal_initial(pDendproximal ptr)
+void proximal_initial(pDenproximal ptr)
 {
     int i;
     for(i = 0; i < DPN; i++)
@@ -15,17 +17,17 @@ void proximal_initial(pDendproximal ptr)
     }
 }
 
-void distal_initial(pDenddistal ptr)
+void distal_initial(pDendistal ptr)
 {
     int i;
     for(i = 0; i < SIZE; i++)
     {
         ptr->boost = 1;
         ptr->overlap = 0;
-        ptr->cell[1] = i % 4;
-        ptr->cell[0] = i / 4;
+        ptr->cell_id[1] = i % 4;
+        ptr->cell_id[0] = i / 4;
         ptr++;
-        
+
     }
 }
 
