38cf8d3f7c5381ca88aa16ac02d6224c3f931333
Tao Heng <uestcheng@gmail.com>
2013-06-19 10:29:04 +0800

    bug fixed
    去掉了无用的id_def函数

--- synapse.c
+++ synapse.c
@@ -12,15 +12,6 @@ void synapse_initial(pSynapse ptr)
     for(i = 0; i < CC; i++)
     {
         ptr->weight = randf();
-        id_def(ptr);
-    }
-}
-
-void id_def(pSynapse ptr)
-{
-    int i;
-    for(i = 0; i < CC; i++)
-    {
         ptr->local_id[0] = i / 4;
         ptr->local_id[1] = i % 4;
         ptr->target_id[1] = ptr->local_id[1];
@@ -30,8 +21,23 @@ void id_def(pSynapse ptr)
         }
         else
         {
-            ptr->target_id[0] = i / 4 - SIZE + 10;
+            ptr->target_id[0] = (i - SIZE) / 4 + 10;
         }
+        ptr++;
+    }
+}
+
+void synapse_show(pSynapse ptr)
+{
+    int i;
+    for(i = 0; i < CC; i++)
+    {
+        printf("weight is %d, local is [%d %d], target_id is [%d %d]",
+               ptr->weight,
+               ptr->local_id[0],
+               ptr->local_id[1],
+               ptr->target_id[0],
+               ptr->target_id[1]);
         ptr++;
     }
 }
